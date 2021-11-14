3f98382912ffc9c489dadd7c23faa7247fd28ebc
lissalad <elissalaymon@gmail.com>
2021-11-13 21:51:22 -0800

    functioning disfunctional/haunted look alike generator

---
+++ data.js
@@ -0,0 +1,64 @@
+const dolls = [
+  {
+    id: 1,
+    name: "Annie",
+    price: 14.49,
+  },
+  {
+    id: 2,
+    name: "Emma",
+    price: 229.9,
+  },
+  {
+    id: 3,
+    name: "Belle",
+    price: 46.21,
+  },
+  {
+    id: 4,
+    name: "Rebecca",
+    price: 25.56,
+  },
+  {
+    id: 5,
+    name: "Elisabeth",
+    price: 346.33,
+  },
+  {
+    id: 6,
+    name: "Beth",
+    price: 3,
+  },
+  {
+    id: 7,
+    name: "Abby",
+    price: 3,
+  },
+  {
+    id: 8,
+    name: "Sarah",
+    price: 3,
+  },
+  {
+    id: 9,
+    name: "Margaret",
+    price: 3,
+  },
+  {
+    id: 10,
+    name: "Claire",
+    price: 3,
+  },
+  {
+    id: 11,
+    name: "Jack",
+    price: 3,
+  },
+  {
+    id: 12,
+    name: "Riley",
+    price: 3,
+  },
+];
+
+export default dolls;
--- scripts.js
+++ scripts.js
@@ -1,12 +1,39 @@
+const generate = document.getElementById("generate");
+const lookAlike = document.getElementById("look-alike");
+const blue = document.getElementById("blue");
+const light = document.getElementById("light");
+const blonde = document.getElementById("blonde");
+const addToCart = document.getElementsByClassName("add-to-cart");
+
 let wbb = [
-  "./images/wbb1",
-  "./images/wbb2",
-  "./images/wbb3",
-  "./images/wbb4",
-  "./images/wbb5",
-  "./images/wbb6",
+  "./images/wbb1.jpg",
+  "./images/wbb2.jpg",
+  "./images/wbb3.jpg",
+  // "./images/wbb4.jpg",
+  // "./images/wbb5.jpg",
+  // "./images/wbb6.jpg",
 ];
+let previous = lookAlike.src;
+
+addToCart.onclick = function (e) {
+  console.log("yes");
+};
+
+generate.onclick = function (e) {
+  lookAlike.src = pickRandom(wbb);
+  // lookAlike.innerHTML = `<img src="${pickRandom(wbb)}" height="300">`;
+  // blue.innerHTML = `<input type="radio" name="eye" id="blue" checked="checked" />`;
+  blue.checked = "checked";
+  light.checked = "checked";
+  blonde.checked = "checked";
+};
 
 function pickRandom(list) {
-  
+  let next = list[Math.floor(Math.random() * list.length)];
+  while (next === previous) {
+    next = list[Math.floor(Math.random() * list.length)];
+  }
+  previous = next;
+  console.log(lookAlike.src);
+  return next;
 }
